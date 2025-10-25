fe393a7c820f85f49b8432837ce82e1c419a4d4b
panejii <faneziaja@gmail.com>
2025-10-25 22:41:50 +0700

    make models for divisi and student

---
+++ app/Models/Student.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $table = 'students';
+    protected $primaryKey = 'id_students';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'nis',
+        'nama',
+        'kelas',
+        'nomor_telepon',
+        'alamat',
+        'nama_ibu',
+        'nama_ayah',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id_students)) {
+                $model->id_students = (string) Str::uuid();
+            }
+        });
+    }
+}
