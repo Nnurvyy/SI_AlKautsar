d933d53262ecc0315744718a4b190f0d9aa3fe24
Nnurvyy <akun217517@gmail.com>
2025-12-01 21:03:59 +0700

    adjust title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="manifest" href="/manifest.json">
     <meta name="theme-color" content="#FFFFFF">
-    <title>{{ config('app.name', 'E-Masjid') }} - @yield('title', 'Selamat Datang')</title>
+    <title>@yield('title') - Smart Masjid</title>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="manifest" href="/manifest.json">
     <meta name="theme-color" content="#FFFFFF">
-    <title>@yield('title', 'E-Masjid')</title>
+    <title>@yield('title') - Smart Masjid</title>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
